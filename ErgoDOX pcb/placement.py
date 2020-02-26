067bd8c510fb14ab20cf73c5b59d08797e0571dc
Jan Mucha <j.mucha@seznam.cz>
2020-02-27 00:46:52 +0100

    placement, routing

--- ErgoDOX pcb/placement.py
+++ ErgoDOX pcb/placement.py
@@ -1,6 +1,52 @@
 import pcbnew
+import math
 
-board = pcbnew.GetBoard()
+board   = pcbnew.GetBoard()
+modules = board.GetModules() 
+
+def rotate(origin, point, angle):
+    """
+    Rotate a point counterclockwise by a given angle around a given origin.
+
+    The angle should be given in radians.
+    """
+    ox = origin.x
+    oy = origin.y
+
+    px = point.x
+    py = point.y
+
+    qx = ox + math.cos(angle) * (px - ox) - math.sin(angle) * (py - oy)
+    qy = oy + math.sin(angle) * (px - ox) + math.cos(angle) * (py - oy)
+
+    return pcbnew.wxPoint(qx, qy)
+    
+def findDiode(id):
+  for module in modules:
+    reference = module.GetReference()
+    if reference.startswith('D'):
+      if reference[1:] == id:
+        return module
+
+
+def postionDiodeForSwitch(switch):
+  switchId       = switch.GetReference()[2:]
+  # print(switchId)
+  diode          = findDiode(switchId)
+  # print(diode.GetReference())
+  switchPosition    = switch.GetPosition()
+  switchOrientation = switch.GetOrientation()
+  swOrDeg = switch.GetOrientationDegrees()
+  print(switchPosition)
+  diodePosition = switchPosition + pcbnew.wxPoint((8300000 * math.sin(math.radians(switchOrientation/10))), (8300000 * math.cos(math.radians(switchOrientation/10))))
+  
+  # diodePosition = rotate(switchPosition, diodePosition, switchOrientation * 10)
+
+  print(switchOrientation)
+
+  diode.SetPosition(diodePosition) #switchPosition.__add__(pcbnew.wxPoint(10, 10)))
+  diode.SetOrientation(switchOrientation)
+
 
 #ds=pcbnew.DRAWSEGMENT(pcb)
 #self._board.Add(ds)
@@ -15,14 +61,16 @@ board = pcbnew.GetBoard()
 
 print("Hello")
 pos = board.GetPosition()
-print("Board position: " + pos)
+print(pos)
 
 
 for module in board.GetModules():
   reference = module.GetReference()
   if (reference.startswith('SW')):
-    print(module.GetReference()),
-    print(module.GetPosition())
+    postionDiodeForSwitch(module)
+    # print(module.GetReference()),
+    # print(module.GetPosition())
 
 
-#pcbnew.Refresh()
+
+pcbnew.Refresh()
